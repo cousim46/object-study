2431dcec6c2f584de7d560debcaa2b8038169426
cousim46 <wjdghldns24@naver.com>
2025-12-18 00:06:45 +0900

    feat : Movie 객체 상태, 행동 정의 및 퍼블릭 인테페이스 생성

--- src/movie/Movie.java
+++ src/movie/Movie.java
@@ -1,5 +1,25 @@
 package movie;
 
-public class Movie {
+import java.time.Duration;
 
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private DisCountPolicy disCountPolicy;
+
+    public Movie(String title, Duration runningTime, Money fee, DisCountPolicy disCountPolicy) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.disCountPolicy = disCountPolicy;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public Money calculateMovieFee(Screeing screeing) {
+        return fee.minus(disCountPolicy.calculateDiscountAmonunt(screeing));
+    }
 }
